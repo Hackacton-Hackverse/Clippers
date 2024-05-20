4d0619faf9fcca6773f470f643289df59a2a8fe5
shadow19walker <franklindavykamgacheuko@gmail.com>
2024-05-20 15:14:36 +0100

    upload the url for the request

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://pipo-app.kesug.com/api',
+    baseURL: 'https://pipo-app.kesug.com/api',
 });
 
 axiosInstance.interceptors.request.use(
